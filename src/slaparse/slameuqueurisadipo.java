4c32828c8739658fa60e1f0c34a2ee14f2c8c7bf
Tecco2233 <biel.tecco@gmail.com>
2019-06-19 11:35:50 -0300

    nsei

---
+++ src/slaparse/slameuqueurisadipo.java
@@ -0,0 +1,15 @@
+package slaparse;
+
+import java.util.*;
+
+public class slameuqueurisadipo {
+	public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner aa = new Scanner(System.in);
+    System.out.println("8uhydsad7uhysadsa");
+    int a = aa.nextInt();
+    int dsa = (a*32131);
+        System.out.println(a + " é " + dsa);
+    }
+
+}
